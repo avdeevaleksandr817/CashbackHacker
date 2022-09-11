d77bda47d9ad6d2f8a9c837e65e9d47133ad51e9
avdeevaleksandr817 <avdeevaleksandr817@gmail.com>
2022-09-12 01:42:00 +0400

    JUnit5 and  Legacy

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -27,4 +27,25 @@ public class CashbackHackServiceTest {
         assertEquals(actual,expected);
     }
 
+    @org.junit.jupiter.api.Test
+    public void shouldMissing50RublesAPI() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 950;
+        int expected = 50;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual,expected);
+    }
+    @org.junit.jupiter.api.Test
+    public void shouldAmountReachAPI() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual,expected);
+    }
+
 }
